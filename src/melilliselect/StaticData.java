f1865403fb33ad91bc7531ea8ace36961e0aa6ef
arsamFarooq <arsam.farooq1@gmail.com>
2023-07-15 14:27:06 +0500

    first commit

---
+++ src/melilliselect/StaticData.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package melilliselect;
+
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
+
+
+/**
+ *
+ * @author arsam
+ */
+public class StaticData {
+    public static int sideNavWidth = 301;
+    public static int folderWidth = 137;
+    public static Color dashboardBackground = new Color(242, 241, 240);
+    public static Color blueBackground = new Color(27, 41, 63);
+//    javax.swing.BorderFactory.createMatteBorder(0, 5, 0, 0, new java.awt.Color(102, 255, 51))
+    public static Color textWhiteColor = new Color(241,242,243);
+    public static Font font12 = new Font("Inter", Font.BOLD, 12);
+    public static Border selectedBorder = javax.swing.BorderFactory.createMatteBorder(0, 5, 0, 0, new java.awt.Color(102, 255, 51));
+}

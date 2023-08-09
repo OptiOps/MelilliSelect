5c5b845f9f0de6076ad73d51aeb146203b355a62
arsamFarooq <arsam.farooq1@gmail.com>
2023-08-09 22:53:01 +0500

    updated

---
+++ AdminMeliliSelect/src/main/java/com/mycompany/adminmeliliselect/AdminMeliliSelect.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.adminmeliliselect;
+
+/**
+ *
+ * @author arsam
+ */
+public class AdminMeliliSelect {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
